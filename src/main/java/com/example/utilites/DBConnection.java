9d48dce19274522aeb9cfbf7481d0d6838c5c3e7
paveltesla <pavellukasenko80@gmail.com>
2023-04-28 01:18:45 +0300

    second commit
    Signed-off-by: paveltesla <pavellukasenko80@gmail.com>

---
+++ src/main/java/com/example/utilites/DBConnection.java
@@ -0,0 +1,22 @@
+package com.example.utilites;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String url = "jdbc:postgresql://localhost:5432/project_db";
+    private static final String name = "postgres";
+    private static final String password = "root";
+
+    public static Connection getConnect() {
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(url, name, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
